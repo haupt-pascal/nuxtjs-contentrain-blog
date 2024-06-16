06e4baf76fe949573811f1164a8e6ddf0892eb33
Pascal Haupt <pascal.haupt@profihost.ag>
2024-06-16 19:19:04 +0200

    added defu due to bugs, improved some stylings

---
+++ components/Navigation.vue
@@ -0,0 +1,36 @@
+<script setup>
+</script>
+<template>
+    <div class="navigation">
+        <div class="navigation__left">
+            <a href="/">
+                <img alt="Logo" class="navigation__logo">
+            </a>
+        </div>
+        <div class="navigation__right">
+            <a href="/blog">Blog</a>
+            <a href="/contact">Contact</a>
+        </div>
+    </div>
+</template>
+<style lang="scss" scoped>
+.navigation {
+    height: 20vh;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 5%;
+
+    .navigation__left {
+
+    }
+
+    .navigation__right {
+        a {
+            margin-left: 20px;
+            color: white;
+            text-decoration: none;
+        }
+    }
+}
+</style>
